65ed27f80f9cba894a9c09fcda8089b1976e5768
WHITING, JEREMY <jeremy.whiting23@gmail.com>
2019-03-26 20:25:15 -0500

    cleanup

---
+++ react-app/build/precache-manifest.5961dc23c72bb093d2df97485b00c6a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7bf30c611b49ffad586",
+    "url": "/static/js/main.c1e8efb4.chunk.js"
+  },
+  {
+    "revision": "a896704c2b272dab0e94",
+    "url": "/static/js/2.36927ab5.chunk.js"
+  },
+  {
+    "revision": "f7bf30c611b49ffad586",
+    "url": "/static/css/main.725a52ef.chunk.css"
+  },
+  {
+    "revision": "f2f900b4b5f26a28db261246deb93992",
+    "url": "/index.html"
+  }
+];
